df66ddf4b14d4e774c824d70c54775aef301bb51
Frank Kair <frankkair@gmail.com>
2025-05-29 08:34:58 +0100

    Removes superfluous comments from plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    '--branch=stable',
     lazypath,
   }
 end
@@ -13,7 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require('pytest_runner').setup()
 require('runfile').setup()
-
 require('lazy').setup({
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
@@ -22,40 +21,28 @@ require('lazy').setup({
   'preservim/nerdtree',
   'akinsho/bufferline.nvim',
   { 'numToStr/Comment.nvim', opts = {} }, -- "gc" to comment regions/lines
-  { 'folke/which-key.nvim', opts = {} }, -- Show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} }, -- Shows pending keybinds
   {
-    -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
+      'williamboman/mason.nvim', -- Automatically installs LSPs to stdpath
       'williamboman/mason-lspconfig.nvim',
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+      { 'j-hui/fidget.nvim', opts = {} }, -- LSP status updates
+      'folke/neodev.nvim', -- Additional Lua configuration
     },
   },
-
   {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
+    'hrsh7th/nvim-cmp', -- Autocompletion
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
+      'L3MON4D3/LuaSnip', -- Snippet engine & its associated nvim-cmp source
       'saadparwaiz1/cmp_luasnip',
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp', -- LSP completion capabilities
       'hrsh7th/cmp-path',
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      'rafamadriz/friendly-snippets', -- User-friendly snippets
     },
   },
-
   {
     'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
     opts = {
       options = {
         icons_enabled = false,
@@ -64,51 +51,34 @@ require('lazy').setup({
       },
     },
   },
-
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
     opts = {},
   },
-
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
+        build = 'make', -- requires local dependencies to be built
         cond = function()
           return vim.fn.executable 'make' == 1
         end,
       },
     },
   },
-
   {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   },
-
   {
-    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
   },
-
 }, {})
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
