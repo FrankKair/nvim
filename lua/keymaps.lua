5bd88e09d34e388411741b8e79e83e9ec17cb93c
Frank Kair <frankkair@gmail.com>
2026-03-21 11:18:44 +0000

    Refactor keymaps & opts
    Examples:
    ```
    local map = vim.keymap.set
    local opt = vim.opt
    ```
    Still unsure whether to inline functions or to extract them for the
    keymaps.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,13 +1,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.diagnostic.config({ virtual_text = false })
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
 
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
@@ -17,8 +13,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- Diagnostic keymaps
-vim.diagnostic.config({ virtual_text = false })
 local function toggle_virtual_text()
   local current = vim.diagnostic.config().virtual_text
   vim.diagnostic.config({ virtual_text = not current })
@@ -28,49 +22,49 @@ local function toggle_line_numbers()
   vim.wo.number = not vim.wo.number
 end
 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-vim.keymap.set('n', '<leader>dt', toggle_virtual_text, { desc = 'Toggle diagnostic virtual text' })
-vim.keymap.set('n', '<leader>ln', toggle_line_numbers, { desc = 'Toggle line numbers' })
+local function open_term()
+  vim.cmd.vnew()
+  vim.cmd.terminal(vim.o.shell)
+end
 
-local opts = { noremap = true, silent = true }
-
--- CMD+s write/save file
-vim.keymap.set('n', '<D-s>', ':w<CR>', opts)
-vim.keymap.set('i', '<D-s>', '<Esc>:w<CR>', opts)
-
--- macOS: CMD+backspace / option+backspace
-vim.keymap.set('i', '<Esc>[DEL-LINE', '<C-u>', opts)
-vim.keymap.set('i', '<M-BS>', '<C-w>', opts)
-
--- Window navigation
-vim.keymap.set('n', '<c-h>', '<c-w>h', opts)
-vim.keymap.set('n', '<c-j>', '<c-w>j', opts)
-vim.keymap.set('n', '<c-k>', '<c-w>k', opts)
-vim.keymap.set('n', '<c-l>', '<c-w>l', opts)
-
--- Splits
-vim.keymap.set('n', 'vs', ':vs<CR>', opts)
-vim.keymap.set('n', 'sp', ':sp<CR>', opts)
-
--- Buffers
-vim.keymap.set('n', '<leader><leader>', '<c-^>', opts)
-vim.keymap.set('n', '<leader>w', ':bp <BAR> bd # <CR>', opts)
-
--- Path (print & copy)
-vim.keymap.set('n', '<leader>pp', ":echo expand('%')<CR>", opts)
-vim.keymap.set('n', '<leader>cp', ":call system('pbcopy', expand('%'))<CR>", opts)
-
+map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Terminal
-vim.keymap.set('n', '<leader>tt', ':vnew term://zsh<CR>', opts)
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
-
+map('n', '<leader>tt', open_term, { desc = 'Open terminal in vertical split' })
+map('t', '<Esc>', '<C-\\><C-n>', opts)
+-- Window navigation
+map('n', '<c-h>', '<c-w>h', opts)
+map('n', '<c-j>', '<c-w>j', opts)
+map('n', '<c-k>', '<c-w>k', opts)
+map('n', '<c-l>', '<c-w>l', opts)
+-- Word wrap
+map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Diagnostics
+map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- Toggling
+map('n', '<leader>dt', toggle_virtual_text, { desc = 'Toggle diagnostic virtual text' })
+map('n', '<leader>ln', toggle_line_numbers, { desc = 'Toggle line numbers' })
+-- CMD+s write/save file
+map('n', '<D-s>', ':w<CR>', opts)
+map('i', '<D-s>', '<Esc>:w<CR>', opts)
+-- macOS: CMD+backspace / option+backspace
+map('i', '<Esc>[DEL-LINE', '<C-u>', opts)
+map('i', '<M-BS>', '<C-w>', opts)
+-- Splits
+map('n', 'vs', ':vs<CR>', opts)
+map('n', 'sp', ':sp<CR>', opts)
+-- Buffers
+map('n', '<leader><leader>', '<c-^>', opts)
+map('n', '<leader>w', ':bp <BAR> bd # <CR>', opts)
+-- Path (print & copy)
+map('n', '<leader>pp', ":echo expand('%')<CR>", opts)
+map('n', '<leader>cp', ":call system('pbcopy', expand('%'))<CR>", opts)
 -- Center search results
-vim.keymap.set('n', 'n', 'nzz', opts)
-vim.keymap.set('n', 'N', 'Nzz', opts)
-vim.keymap.set('n', '*', '*zz', opts)
-vim.keymap.set('n', '#', '#zz', opts)
-vim.keymap.set('n', 'g*', 'g*zz', opts)
-
+map('n', 'n', 'nzz', opts)
+map('n', 'N', 'Nzz', opts)
+map('n', '*', '*zz', opts)
+map('n', '#', '#zz', opts)
+map('n', 'g*', 'g*zz', opts)
 -- Clear search highlight on Enter
-vim.keymap.set('n', '<CR>', ':nohlsearch<CR>', opts)
+map('n', '<CR>', ':nohlsearch<CR>', opts)
